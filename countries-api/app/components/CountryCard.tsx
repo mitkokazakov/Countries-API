44555a67c6fec93bf1df8fbe842d69af52ad95fc
mitkokazakov <DimitarKazakov@students.softuni.bg>
2024-05-09 13:11:51 +0300

    Style home page for displaying all countries on large screens

--- countries-api/app/components/CountryCard.tsx
+++ countries-api/app/components/CountryCard.tsx
@@ -10,8 +10,8 @@ type CountryParams = {
 
 const CountryCard = ({name,population,region,capital,flag}: CountryParams) => {
   return (
-    <div className='flex flex-col rounded-lg shadow-md overflow-hidden'>
-      <img src={flag} alt="" />
+    <div className='flex flex-col rounded-lg shadow-md overflow-hidden '>
+      <img className='lg:w-full lg:max-h-[160px] lg:min-h-[160px]' src={flag} alt="" />
 
       <div className='flex flex-col justify-between items-start pl-6 pt-8 pb-10 dark:bg-[#2b3743] dark:text-white'>
         <h1 className='text-xl font-extrabold pb-4'>{name}</h1>
--- countries-api/app/page.tsx
+++ countries-api/app/page.tsx
@@ -91,39 +91,41 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start pt-10 px-4 bg-[#fafafa] font-nunito dark:bg-[#202d36]">
+    <main className="flex min-h-screen flex-col items-center justify-start pt-10 px-4 bg-[#fafafa] font-nunito dark:bg-[#202d36] lg:px-10">
 
-      <div className="w-full shadow-md rounded-lg relative">
-        <IoIosSearch className="absolute left-5 top-[50%] translate-y-[-50%] text-2xl text-slate-400" />
-        <input className="w-full py-3 pl-16 pr-4 rounded-lg outline-none text-xs dark:text-white dark:bg-[#2b3743]" type="text" name="title" placeholder="Search for a country..." onChange={OnChangeInput} />
-      </div>
+      <div className="flex flex-col w-full lg:flex-row lg:justify-between lg:items-center">
+        <div className="w-full shadow-md rounded-lg relative lg:w-2/5">
+          <IoIosSearch className="absolute left-5 top-[50%] translate-y-[-50%] text-2xl text-slate-400" />
+          <input className="w-full py-3 pl-16 pr-4 rounded-lg outline-none text-xs dark:text-white dark:bg-[#2b3743]" type="text" name="title" placeholder="Search for a country..." onChange={OnChangeInput} />
+        </div>
 
-      <div className="mt-10 w-full">
-        <div className=" w-2/4 relative">
-          <div className="w-full flex justify-between items-center pl-6 pr-4 py-3 bg-white rounded-md shadow-md dark:bg-[#2b3743]">
-            <p className="text-[10px] font-medium dark:text-white">Filter By Region</p>
-            <MdKeyboardArrowDown className="text-md cursor-pointer dark:text-white" onClick={() => { setClickRegion(!clickRegion) }} />
-          </div>
+        <div className="mt-10 w-full lg:mt-0 lg:w-1/4">
+          <div className=" w-2/4 relative lg:w-full">
+            <div className="w-full flex justify-between items-center pl-6 pr-4 py-3 bg-white rounded-md shadow-md dark:bg-[#2b3743]">
+              <p className="text-[10px] font-medium dark:text-white">Filter By Region</p>
+              <MdKeyboardArrowDown className="text-md cursor-pointer dark:text-white" onClick={() => { setClickRegion(!clickRegion) }} />
+            </div>
 
-          <div className={`w-full duration-200 bg-white dark:bg-[#2b3743] mt-2 absolute shadow-md ${clickRegion ? 'h-auto opacity-1 pl-6 py-4 pointer-events-auto' : 'h-0 opacity-0 p-0 pointer-events-none'}`}>
-            <p className="py-1 text-[14px] font-medium dark:text-white cursor-pointer" onClick={() => OnClickRegion("Africa")}>Africa</p>
-            <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("America")}>America</p>
-            <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Asia")}>Asia</p>
-            <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Europe")}>Europe</p>
-            <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Oceania")}>Oceania</p>
+            <div className={`w-full duration-200 bg-white dark:bg-[#2b3743] mt-2 absolute shadow-md ${clickRegion ? 'h-auto opacity-1 pl-6 py-4 pointer-events-auto' : 'h-0 opacity-0 p-0 pointer-events-none'}`}>
+              <p className="py-1 text-[14px] font-medium dark:text-white cursor-pointer" onClick={() => OnClickRegion("Africa")}>Africa</p>
+              <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("America")}>America</p>
+              <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Asia")}>Asia</p>
+              <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Europe")}>Europe</p>
+              <p className="py-1 text-[14px] font-medium  dark:text-white cursor-pointer" onClick={() => OnClickRegion("Oceania")}>Oceania</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-8 px-8 mt-10">
+      <div className=" flex flex-col gap-8 px-8 mt-10 md:flex-row md:justify-between md:items-center md:flex-wrap lg:px-0 lg:flex-row lg:flex-wrap lg:justify-between lg:items-center">
 
 
         {
 
           filterByRegionCountries.length != 0 ? filterByRegionCountries.map((c: Country, index) => {
             return <>
-              <Link href={`/country/${c.name.common}`} key={index}>
-                <CountryCard name={c.name.common} population={c.population} region={c.region} capital={c.capital} flag={c.flags.png}/>
+              <Link className="md:w-1/4 lg:w-1/5" href={`/country/${c.name.common}`} key={index}>
+                <CountryCard name={c.name.common} population={c.population} region={c.region} capital={c.capital} flag={c.flags.png} />
               </Link>
             </>
           }) : null
