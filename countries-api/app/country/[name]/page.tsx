80699e2e68a07a4cb4da1350880196a23bff0613
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-06-05 14:35:25 +0300

    Change css for Country details page for large screens

--- countries-api/app/country/[name]/page.tsx
+++ countries-api/app/country/[name]/page.tsx
@@ -4,36 +4,63 @@ import { FaArrowLeftLong } from "react-icons/fa6";
 
 import { ExtractNativeName,ExtractCurrencies, ExtractLanguages, FormatPopulation } from '@/app/libs/services';
 
+import jsonData from '../../libs/data.json';
+
 type ParamsType = {
     params: {
         name: string
     }
 }
 
+type Country = {
+    name: string;
+    region: string;
+    subregion: string;
+    nativeName: string;
+    topLevelDomain: [];
+    currencies: [
+        {
+            name: string
+        }
+    ];
+    languages: [
+        {
+            name: string
+        }
+    ];
+    population: number;
+    flags: {
+      png: string;
+    };
+    capital: string;
+  };
+
 
 const page = async ({ params }: ParamsType) => {
+
+    let data: any = jsonData;
 
     let splittedName = params.name.split('%20');
 
     let finalName = splittedName.join(' ');
 
 
-    const response = await fetch(`https://restcountries.com/v3.1/name/${finalName.toLocaleLowerCase()}`);
+    let currentCountry = data.filter((country: Country) => country.name.toLowerCase() == finalName.toLowerCase());
 
-    const data = await response.json();
+    // const response = await fetch(`https://restcountries.com/v3.1/name/${finalName.toLocaleLowerCase()}`);
 
-    const country = data[0];
+    // const data = await response.json();
 
-    const nativeName = ExtractNativeName(country);
+    const country = currentCountry[0];
 
-    const currenciesString = ExtractCurrencies(country);
+    //const currenciesString = ExtractCurrencies(country);
 
-    const languagesString = ExtractLanguages(country);
+    //const languagesString = ExtractLanguages(country);
     
     const formattedPopulation = FormatPopulation(country.population);
 
     return (
-        <div className=' flex flex-col px-8 py-8 bg-[#fafafa] dark:bg-[#202d36] min-h-screen'>
+        <div className=' flex flex-col px-8 py-8 bg-[#fafafa] dark:bg-[#202d36] min-h-screen max-w-[1900px]'>
 
             <div className='flex justify-start '>
                 <Link href={'/'} className=' flex justify-center items-center gap-3 w-32 shadow-3xl py-2 outline-none dark:text-white dark:bg-[#2b3743]'>
@@ -48,13 +75,13 @@ const page = async ({ params }: ParamsType) => {
                     <img className='w-full' src="https://flagcdn.com/w320/ax.png" alt="Country Image" />
                 </div>
 
-                <div className='flex flex-col dark:text-white lg:justify-center lg:items-center lg:w-1/2 lg:py-5'>
-                    <div className='w-full flex flex-col mt-10 md:flex-row md:justify-evenly md:items-start  lg:justify-between '>
+                <div className='flex flex-col dark:text-white lg:justify-center lg:items-start lg:w-1/2 lg:py-5 '>
+                    <div className='w-full flex flex-col mt-10 md:flex-row md:justify-evenly md:items-start  lg:justify-start'>
                         <div className='flex flex-col'>
                             <h1 className='text-2xl font-extrabold pb-4'>{finalName}</h1>
 
                             <div className='flex flex-col gap-3'>
-                                <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Native Name: </span>{nativeName.common}</p>
+                                <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Native Name: </span>{country.nativeName}</p>
                                 <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Population: </span> {formattedPopulation}</p>
                                 <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Region: </span> {country.region}</p>
                                 <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Sub Region: </span> {country.subregion}</p>
@@ -62,10 +89,10 @@ const page = async ({ params }: ParamsType) => {
                             </div>
                         </div>
 
-                        <div className='flex flex-col gap-3 mt-8 md:pt-4'>
-                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Top Level Domain: </span> {country.tld[0]}</p>
-                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Currencies: </span>{currenciesString}</p>
-                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Languages: </span>{languagesString}</p>
+                        <div className='flex flex-col gap-3 mt-8 md:pt-4 '>
+                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Top Level Domain: </span> {country.topLevelDomain[0]}</p>
+                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Currencies: </span>Euro</p>
+                            <p className=' font-extralight lg:text-sm'><span className=' font-bold'>Languages: </span>Norsk</p>
                         </div>
 
                     </div>
