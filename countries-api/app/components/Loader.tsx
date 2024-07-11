739878de8e9bf811836d45d5beff766e5b62b9d6
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-07-11 07:49:22 +0300

    Change loader background color

--- countries-api/app/components/Loader.tsx
+++ countries-api/app/components/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className=" min-h-screen w-full grid items-center bg-slate-100">
+    <div className=" min-h-screen w-full grid items-center bg-[#fafafa] font-nunito dark:bg-[#202d36]">
       <div className="spinner">
         <div className="rect1"></div>
         <div className="rect2"></div>
