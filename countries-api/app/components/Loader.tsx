61f72141cbe1ecf753b3ec0f0317c3949774a68a
Dimitar Kazakov <62236384+mitkokazakov@users.noreply.github.com>
2024-07-11 07:55:05 +0300

    Change color of every single element of loader

--- countries-api/app/components/Loader.tsx
+++ countries-api/app/components/Loader.tsx
@@ -4,11 +4,11 @@ const Loader = () => {
   return (
     <div className=" min-h-screen w-full grid items-center bg-[#fafafa] font-nunito dark:bg-[#202d36]">
       <div className="spinner">
-        <div className="rect1"></div>
-        <div className="rect2"></div>
-        <div className="rect3"></div>
-        <div className="rect4"></div>
-        <div className="rect5"></div>
+        <div className="rect1 bg-[#333] dark:bg-[#fafafa]"></div>
+        <div className="rect2 bg-[#333] dark:bg-[#fafafa]"></div>
+        <div className="rect3 bg-[#333] dark:bg-[#fafafa]"></div>
+        <div className="rect4 bg-[#333] dark:bg-[#fafafa]"></div>
+        <div className="rect5 bg-[#333] dark:bg-[#fafafa]"></div>
       </div>
     </div>
   )
